5477bfe6860534e5030d9bf3d707a88844b59fd0
Дудорова Ульяна ТКИ-142 <uliana.dudorova@gmail.com>
2026-05-01 19:18:14 +0300

    Исправила Color.cpp

--- Color.cpp
+++ Color.cpp
@@ -1,28 +1,30 @@
-﻿#include "Color.h"
+#include "Color.h"
 
 using namespace std;
 
-Color::Color(int r, int g, int b) {
-    if (r < 0 || r > 255 || g < 0 || g > 255 || b < 0 || b > 255) {
-        throw out_of_range("Компоненты цвета должны быть в диапазоне от 0 до 255");
-    }
-    red = static_cast<unsigned int>(r);
-    green = static_cast<unsigned int>(g);
-    blue = static_cast<unsigned int>(b);
+Color::Color(const int r, const int g, const int b) : r(r), g(g), b(b) {}
+
+void Color::SetR(const int value) {
+    if (value < 0 || value > 255) throw out_of_range("R must be 0-255");
+    r = value;
+}
+
+void Color::SetG(const int value) {
+    if (value < 0 || value > 255) throw out_of_range("G must be 0-255");
+    g = value;
+}
+
+void Color::SetB(const int value) {
+    if (value < 0 || value > 255) throw out_of_range("B must be 0-255");
+    b = value;
 }
 
 ostream& operator<<(ostream& os, const Color& color) {
-    os << "RGB(" << color.red << ", " << color.green << ", " << color.blue << ")";
+    os << "RGB(" << color.r << "," << color.g << "," << color.b << ")";
     return os;
 }
 
 istream& operator>>(istream& is, Color& color) {
-    int r, g, b;
-    while (!(is >> r >> g >> b)) {
-        is.clear();
-        is.ignore(numeric_limits<streamsize>::max(), '\n');
-        cerr << "Неверный ввод. Введите значения RGB (красный зеленый синий): ";
-    }
-    color = Color(r, g, b);
+    is >> color.r >> color.g >> color.b;
     return is;
 }
