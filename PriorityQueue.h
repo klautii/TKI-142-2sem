d0babe26a1a54cb2b577c76d4681cb215b7db259
Дудорова Ульяна ТКИ-142 <uliana.dudorova@gmail.com>
2026-05-02 13:55:45 +0300

    3 задание

---
+++ PriorityQueue.h
@@ -0,0 +1,139 @@
+﻿#pragma once﻿
+#include <initializer_list>
+#include <utility>
+#include <string>
+
+using namespace std;
+
+class PriorityQueue {
+private:
+    /**
+     * @brief Узел очереди
+     */
+    struct Node {
+        int value = 0;
+        int priority;
+        Node* next;
+        Node* prev;
+
+        /**
+         * @brief Конструктор узла
+         * @param val Значение элемента
+         * @param prio Приоритет элемента
+         */
+        Node(int prio) : priority(prio), next(nullptr), prev(nullptr) {}
+    };
+
+    Node* head;
+    Node* tail;
+    size_t size;
+
+public:
+
+    /**
+     * @brief Конструктор по умолчанию
+     */
+    PriorityQueue();
+
+    /**
+     * @brief Конструктор из списка инициализации
+     * @param initList Список пар {значение, приоритет} для инициализации
+     */
+    PriorityQueue(initializer_list<pair<int, int>> initList);
+
+    /**
+     * @brief Конструктор копирования
+     * @param other Очередь для копирования
+     */
+    PriorityQueue(const PriorityQueue& other);
+
+    /**
+    * @brief Конструктор перемещения
+    * @param other Очередь для перемещения
+    */
+    PriorityQueue(PriorityQueue&& other) noexcept;
+
+    /**
+     * @brief Деструктор
+     */
+    ~PriorityQueue();
+
+    /**
+     * @brief Оператор присваивания копированием
+     * @param other Очередь для копирования
+     * @return Ссылка на текущий объект
+     */
+    PriorityQueue& operator=(const PriorityQueue& other);
+
+    /**
+     * @brief Оператор присваивания перемещением
+     * @param other Очередь для перемещения
+     * @return Ссылка на текущий объект
+     */
+    PriorityQueue& operator=(PriorityQueue&& other) noexcept;
+
+    /**
+     * @brief Оператор вставки элемента
+     * @param element Пара {значение, приоритет} для вставки
+     * @return Ссылка на текущий объект
+     */
+    PriorityQueue& operator<<(const pair<int, int>& element);
+
+    /**
+     * @brief Оператор извлечения элемента с максимальным приоритетом
+     * @param element Ссылка для сохранения извлеченного элемента
+     * @return Ссылка на текущий объект
+     */
+    PriorityQueue& operator>>(pair<int, int>& element);
+
+    /**
+     * @brief Добавление элемента в очередь
+     * @param value Значение элемента
+     * @param priority Приоритет элемента
+     */
+    void push(int value, int priority);
+
+    /**
+     * @brief Извлечение элемента с максимальным приоритетом
+     * @param value Ссылка для сохранения извлеченного значения
+     * @return true если элемент был извлечен, false если очередь пуста
+     */
+    bool popMax(int& value);
+
+    /**
+     * @brief Просмотр элемента с максимальным приоритетом без извлечения
+     * @param value Ссылка для сохранения значения
+     * @return true если элемент существует, false если очередь пуста
+     */
+    bool peekMax(int& value) const;
+
+    /**
+     * @brief Просмотр элемента с минимальным приоритетом без извлечения
+     * @param value Ссылка для сохранения значения
+     * @return true если элемент существует, false если очередь пуста
+     */
+    bool peekMin(int& value) const;
+
+    /**
+    * @brief Проверка очереди на пустоту
+    * @return true если очередь пуста, false в противном случае
+    */
+    bool isEmpty() const;
+
+    /**
+     * @brief Получение текущего размера очереди
+     * @return Количество элементов в очереди
+     */
+    size_t getSize() const;
+
+    /**
+     * @brief Преобразование очереди в строку
+     * @return Строковое представление очереди
+     */
+    string toString() const;
+
+    /**
+     * @brief Очистка очереди
+     */
+    void clear();
+};
---
+++ main.cpp
@@ -0,0 +1,44 @@
+﻿#include "../decision/PriorityQueue.h"
+#include <iostream>
+#include <locale>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    PriorityQueue pq1 = { {10, 3}, {20, 1}, {30, 5}, {40, 2} };
+    cout << "Начальная очередь: " << pq1.toString() << endl;
+
+    pq1.push(50, 4);
+    pq1 << make_pair(60, 0);
+    cout << "После добавления элементов: " << pq1.toString() << endl;
+
+    int value;
+    cout << "\nИзвлечение элементов с максимальным приоритетом:" << endl;
+    while (pq1.popMax(value)) {
+        cout << "Извлеченный максимум: " << value << endl;
+    }
+
+    cout << "\nОчередь пуста: " << boolalpha << pq1.isEmpty() << endl;
+
+    PriorityQueue pq2;
+    pq2.push(100, 10);
+    pq2.push(200, 5);
+    pq2.push(300, 20);
+    cout << "\nВторая очередь: " << pq2.toString() << endl;
+
+
+    if (pq2.peekMax(value)) {
+        cout << "Текущий максимум: " << value << endl;
+    }
+
+    PriorityQueue pq3 = pq2;
+    cout << "\nСкопированная очередь: " << pq3.toString() << endl;
+
+    pair<int, int> element;
+    pq3 >> element;
+    cout << "Извлечено через оператор >>: (" << element.first << ", " << element.second << ")" << endl;
+
+    return 0;
+}
